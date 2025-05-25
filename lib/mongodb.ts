0754744655f05523495bb2f712c4a62ae96e4c22
Justin Couch <justincouch1@gmail.com>
2025-05-24 21:24:55 -0500

    Fix: type-safe global MongoClient promise for Next.js

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -3,32 +3,31 @@ import { MongoClient } from 'mongodb';
 const uri = process.env.MONGODB_URI;
 const options = {};
 
-declare global {
-  namespace NodeJS {
-    interface Global {
-      _mongoClientPromise?: Promise<MongoClient>;
-    }
-  }
-}
-
-let clientPromise: Promise<MongoClient>;
-
 if (!uri) {
   throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
 }
 
+let client: MongoClient;
+let clientPromise: Promise<MongoClient>;
+
 if (process.env.NODE_ENV === 'development') {
+  // @ts-ignore
   if (!global._mongoClientPromise) {
-    const client = new MongoClient(uri, options);
+    client = new MongoClient(uri, options);
+    // @ts-ignore
     global._mongoClientPromise = client.connect();
   }
+  // @ts-ignore
   clientPromise = global._mongoClientPromise;
 } else {
-  const client = new MongoClient(uri, options);
+  client = new MongoClient(uri, options);
   clientPromise = client.connect();
 }
 
 export default clientPromise;
+
+
+
 
 
 
