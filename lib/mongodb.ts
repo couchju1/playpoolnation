483d3e0d488b0cf6bed8e2ca4af92784f68a4c71
Justin Couch <justincouch1@gmail.com>
2025-05-24 22:51:33 -0500

    Fix MongoDB connection issue for Vercel

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -3,10 +3,16 @@ import { MongoClient } from 'mongodb';
 const uri = process.env.MONGODB_URI;
 const options = {};
 
+// Extend globalThis to hold the cached connection during dev
+declare global {
+  interface GlobalThis {
+    _mongoClientPromise?: Promise<MongoClient>;
+  }
+}
+
 if (!uri) {
   throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
 }
-
 let client: MongoClient;
 let clientPromise: Promise<MongoClient>;
 
@@ -20,7 +26,8 @@ if (process.env.NODE_ENV === 'development') {
   // @ts-expect-error: reading undeclared global prop
   clientPromise = global._mongoClientPromise;
 } else {
-  client = new MongoClient(uri, options);
+  // Always create a new client in production
+  const client = new MongoClient(uri, options);
   clientPromise = client.connect();
 }
 
